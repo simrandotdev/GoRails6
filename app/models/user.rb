2134c7733104ade49e94cd44875303213dc9a183
Simran Preet Singh <simran5590@gmail.com>
2022-12-10 19:53:42 +0530

    Installing bcrypt and securing password in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# email: string
+# password_digest:string
+# password: string `virtual attribute`
+# password_confirmation: string `virtual attribute`
 class User < ApplicationRecord
+  # These are virtual attributes that won't get saved to the database
+  # but when we submit a password it will run through bcrypt
+  # and hash that password.
+  has_secure_password
 end
