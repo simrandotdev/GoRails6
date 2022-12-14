c18d276df4104d240e176dfda9c47a37cdb2506c
Simran Preet Singh <simran5590@gmail.com>
2022-12-14 16:30:34 +0530

    Using Current.user to check if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  # Before any action runs, this will get called
+  before_action :set_current_user
+
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find(session[:user_id])
+    end
+  end
+
 end
