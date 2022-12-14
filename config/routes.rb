2fbff0055021d17b346d05a76f132d01b63a99b7
Simran Preet Singh <simran5590@gmail.com>
2022-12-14 16:32:36 +0530

    added some spacing to categorize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
+
   # Users Routes
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
+
   get "logout", to: "registration#logout"
+
   get "login", to: "sessions#new"
   post "login", to: "sessions#login"
   # Root Route
